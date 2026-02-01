46fa41c17436356c219babeb35611bdea87337d0
BenIsRong <benisrong@gmail.com>
2026-02-01 19:14:38 +0800

    fix: changed file names

--- counting_sort.py
+++ counting_sort.py
@@ -1,5 +1,4 @@
 import random
-import timeit
 
 def counting_sort(input):
     if not input:
@@ -25,49 +24,10 @@ def counting_sort(input):
 
     return output
 
-normal_counting_sort_correct = []
-def normal_counting_sort(max_value):
+
+if __name__ == "__main__":
+    max_value = 1000
     input = [random.randint(0, max_value) for _ in range(max_value)]
     print(f"Input list: {input}")
     output = counting_sort(input)
     print(f"Output list: {output}")
-    normal_counting_sort_correct.append({"input": input, "output": output})
-
-n_squared_counting_sort_correct = []
-def n_squared_counting_sort(max_value):
-    input = [random.randint(0, max_value * max_value) for _ in range(max_value)]
-    print(f"Input list: {input}")
-    output = counting_sort(input)
-    print(f"Output list: {output}")
-    n_squared_counting_sort_correct.append({"input": input, "output": output})
-
-def test_correctness(list_of_tests):
-    correct = 0
-    for test in list_of_tests:
-        correct_sort = sorted(test["input"])
-        if test["output"] == correct_sort:
-            correct += 1
-
-    return correct
-
-if __name__ == "__main__":
-    max_value = 1000
-    num_rounds = 100
-
-    normal = timeit.timeit("normal_counting_sort(1000)", "from __main__ import normal_counting_sort", number=num_rounds)
-    n_squared = timeit.timeit("n_squared_counting_sort(1000)", "from __main__ import n_squared_counting_sort", number=num_rounds)
-
-
-    print("\ncalculating...")
-    normal_correctness = test_correctness(normal_counting_sort_correct)
-    n_squared_correctness = test_correctness(n_squared_counting_sort_correct)
-
-    print("\ndone calculating")
-    print(f"n (average): {normal:.2f} seconds")
-    print(f"n squared (average): {n_squared:.2f} seconds")
-    print(f"n squared took around {(n_squared/normal):.2f} times longer than n")
-
-    print(f"\nn correct: {normal_correctness}/{num_rounds}")
-    print(f"n squared correct: {n_squared_correctness}/{num_rounds}")
-
-    
